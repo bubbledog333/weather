02b50d259d040f2e9814a2f52c86521eb7867c07
bubbledog333 <95395175+bubbledog333@users.noreply.github.com>
2021-12-12 16:34:29 -1000

    added icon

--- indelnew.js
+++ indelnew.js
@@ -35,7 +35,10 @@ function formatDate(date) {
     );
     document.querySelector("#description").innerHTML =
       response.data.weather[0].main;
-  }
+  
+  let icon = document.querySelector(img) 
+icon.innerHTML = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+}
   
   function searchCity(city) {
     let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
