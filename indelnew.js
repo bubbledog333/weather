25c8cbd0689d09cd0486622f21732de58a9ea313
bubbledog333 <95395175+bubbledog333@users.noreply.github.com>
2021-12-12 17:01:38 -1000

    fixed icon... ( I think...)

--- indelnew.js
+++ indelnew.js
@@ -36,8 +36,9 @@ function formatDate(date) {
     document.querySelector("#description").innerHTML =
       response.data.weather[0].main;
   
-  let icon = document.querySelector(img) 
-icon.innerHTML = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  let icon = document.querySelector(img) ;
+icon.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+icon.setAttribute("alt",response.data.weather[0].description);
 }
   
   function searchCity(city) {
